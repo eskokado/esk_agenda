0aa38f96b778a45460bad49edd5b9c0d0c096a87
eskokado <eskokado@gmail.com>
2023-01-27 09:15:10 -0300

    feat: add address in form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,6 +13,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    @contact.build_address
     options_for_select
   end
 
@@ -67,7 +68,13 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :kind_id, :rmk)
+      params.require(:contact).permit(
+        :name, 
+        :email, 
+        :kind_id, 
+        :rmk, 
+        address_attributes: [:street, :city, :state]
+      )
     end
 
     def options_for_select
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_one :address
 
-  accept_nested_attibutes_for :address
+  accepts_nested_attributes_for :address
 end
